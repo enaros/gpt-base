b71acef89ce28ebfdd4a0e8abcf78c13f6ad5069
emiliano-onorati <emiliano.onorati@agrinorm.ai>
2023-12-04 18:46:19 +0700

    message component

--- app/components/Message/Message.tsx
+++ app/components/Message/Message.tsx
@@ -1,10 +1,11 @@
-import cn from 'classnames';
-import {useEffect, useRef, useState} from 'react';
-import ReactMarkdown from 'react-markdown';
+import cn from "classnames";
+import ReactMarkdown from "react-markdown";
 
-import {Assistant as AssistantIcon, User as UserIcon} from '~/components/Icons';
+import useMessageAnimation from "~/hooks/useMessageAnimation";
 
-import {Role} from '~/types';
+import { Role } from "~/types";
+import userImg from "~/img/user.png";
+import TransactionList from "../TransactionList/TransactionList";
 
 export interface MessageProps {
   content: string;
@@ -17,68 +18,46 @@ export interface MessageProps {
 /**
  * Renders a message
  */
-export default function Message({content, error, role = 'user', thinking = false}: MessageProps) {
-  const rendered = useRef<null | boolean>(null)
-  const messageRef = useRef<HTMLDivElement>(null)
-  const messageWrapRef = useRef<HTMLDivElement>(null)
-  const [messageHeight, setMessageHeight] = useState(0);
+export default function Message({
+  content,
+  error,
+  role = "user",
+  thinking = false,
+}: MessageProps) {
+  const { rendered, messageRef, messageWrapRef, messageHeight } =
+    useMessageAnimation();
 
-  const handleTransitionEnd = () => {
-    const theMessageWrap = messageWrapRef.current;
-    if (!theMessageWrap) {
-      return;
-    }
-
-    theMessageWrap.removeAttribute('style');
-  }
-
-  useEffect(() => {
-    const theMessage = messageRef.current;
-
-    if (rendered.current !== null || !theMessage) {
-      return
-    }
-
-    rendered.current = true;
-
-    const contentHeight = theMessage.scrollHeight;
-    const containerStyle = window.getComputedStyle(theMessage.parentElement!);
-    const paddingTop = parseFloat(containerStyle.paddingTop);
-    const paddingBottom = parseFloat(containerStyle.paddingBottom);
-    const totalHeight = contentHeight + paddingTop + paddingBottom;
-
-    setMessageHeight(totalHeight);
-  }, []);
-
-  useEffect(() => {
-    const theMessageWrap = messageWrapRef.current;
-    if (!theMessageWrap) {
-      return;
-    }
-
-    theMessageWrap.addEventListener('transitionend', handleTransitionEnd);
-
-    return () => {
-      theMessageWrap.removeEventListener('transitionend', handleTransitionEnd);
-    };
-  }, []);
+  if (content === "### account balance ###") return <TransactionList />;
 
   return (
     <div
       className={cn(
-        `message message-${role} w-full flex small-mobile:max-sm:text-sm transition-height duration-300`,
-        role === 'user' ? 'justify-end' : 'justify-start',
-        error && 'text-error'
+        `message message-${role} flex small-mobile:max-sm:text-sm -transition-height -duration-300 justify-left`,
+        // role === 'user' ? 'justify-end' : 'justify-start',
+        thinking ? "w-[100px]" : "",
+        error && "text-error"
       )}
       ref={messageWrapRef}
-      style={{height: `${messageHeight}px`, overflow: 'hidden'}}
+      style={{ height: `${messageHeight}px`, overflow: "hidden" }}
     >
-      <div className={cn(
-        'message-inner space-x-4 max-w-[480px] rounded-3xl p-4 transition-[opacity, transform] duration-300 delay-250 relative text-sm md:text-base',
-        role === 'user' ? 'bg-black text-white rounded-br-none' : ' rounded-tl-none bg-slate-100 text-black',
-        rendered.current ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-7',
-      )}>
-        <div className="response" ref={messageRef}>
+      <div
+        className={cn(
+          "message-inner w-full space-x-4 max-w-[500px] rounded-3xl p-5 pl-8 pr-8 transition-[opacity, transform] duration-300 delay-250 relative text-sm md:text-base",
+          role === "user" ? "bg-none text-gray-700" : "bg-white text-black",
+          rendered.current
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 translate-y-7"
+        )}
+      >
+        <div
+          className={cn("response", role === "user" ? "flex items-center" : "")}
+          ref={messageRef}
+        >
+          {role === "user" && (
+            <div className="user">
+              <img src={userImg} />
+            </div>
+          )}
           {thinking ? (
             <div className="flex gap-[5px] min-h-[20px] sm:min-h-[24px] items-center">
               <span className="thinking-bubble animate-thinking-1" />
@@ -86,10 +65,12 @@ export default function Message({content, error, role = 'user', thinking = false
               <span className="thinking-bubble animate-thinking-3" />
             </div>
           ) : (
-            <ReactMarkdown children={content} />
+            <div>
+              <ReactMarkdown children={content} />
+            </div>
           )}
         </div>
       </div>
     </div>
-  )
+  );
 }
